b63621528a2305379952f89e7439e881d2adbeab
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-03-26 18:24:25 -0400

    returning new list

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const uuid = require("./helpers/uuid");
 const app = express();
 //created a server
 const PORT = process.env.PORT || 3001;
@@ -34,25 +34,56 @@ app.get("/api/notes", (req, res) => {
     res.send(JSON.parse(data));
   });
 });
-//POST. add new notes to the array
-app.post("api/notes", (req, res) => {});
 
+//POST. add new notes to the array
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+
+  if (req.body) {
+    const newNote = {
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    console.log(newNote);
+    readAndAppend(newNote, "./db/db.json");
+  }
+});
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 //delete notes
 app.delete("/api/notes/:id", (req, res) => {
   console.log("DELETE note with id: " + req.params.id);
+  updateNotesDb();
+});
 
+const updateNotesDb = () => {
   const data = fs.readFileSync("./db/db.json");
   const noteList = JSON.parse(data);
 
-  updatedNoteList = noteList.filter((note) => {
+  let updatedNoteList = noteList.filter((note) => {
     return note.id !== req.params.id;
   });
 
   fs.writeFileSync("./db/db.json", JSON.stringify(updatedNoteList));
   res.send("delete successful");
-});
+};
 
 //port
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
